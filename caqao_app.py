0b3c3d1c24666602f67294ef381161a28ecec223
fvea <fjvinceatabay@gmail.com>
2023-04-03 05:27:04 +0800

    add updates

--- caqao_app.py
+++ caqao_app.py
@@ -1,28 +1,23 @@
 from flask import Flask, request, send_file, jsonify
+from werkzeug.security import check_password_hash, generate_password_hash
 from PIL import Image
+import socket
+from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity, create_access_token
+
 
 from urllib.parse import urlparse
-from datetime import datetime
-import socket
 import torch
+torch.set_num_threads(1)
 import io
 import os
 import secrets
 import uuid
 import random
 
+
 from db import db_init, db
-from model import Detection, TempDetection
+from model import Detection, TempDetection, User
 
-
-DATETIME_FORMAT = "%Y-%m-%d_%H-%M-%S-%f"
-IMAGE_URL = "http://{}:5000/detections/{}"
-PREVIEW_IMAGE_URL = "http://{}:5000/preview/{}"
-TEMP_DIR = "temp"
-
-hostname = socket.gethostname()
-ip_address = socket.gethostbyname(hostname)
-FLASK_IP_ADDR = ip_address
 
 app = Flask(__name__)
 # SQLAlchemy config. Read more: https://flask-sqlalchemy.palletsprojects.com/en/2.x/
@@ -31,10 +26,17 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///detections.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db_init(app)
 
+jwt = JWTManager(app)
+
+app.config.update(SESSION_COOKIE_SAMESITE="None", SESSION_COOKIE_SECURE=True)
+
+MAX_DET = 50
+model = torch.hub.load('ultralytics/yolov5', 'custom', path="./best.pt", force_reload=True)
+model.max_det = MAX_DET
 
 @app.route("/assess", methods=["POST"])
 def assess():
-    
+
     if request.method == "POST":
 
         # get cacao beans image and bean size from request
@@ -67,11 +69,12 @@ def assess():
         )
         db.session.add(temp_detection)
         db.session.commit()
-        
+
     return get_json_response(temp_detection)
-    
+
 
 @app.route("/save_results", methods=["POST"])
+@jwt_required()
 def save_results():
 
     if request.method == "POST":
@@ -79,10 +82,12 @@ def save_results():
 
         image_src_url = str(request.form["imgSrcUrl"])
         filename = os.path.basename(urlparse(image_src_url).path)
-        print(image_src_url)
+
+        user_id = get_jwt_identity()
 
         temp_detection = TempDetection.query.filter_by(filename=filename).first()
         detection = Detection(
+            user_id=user_id,
             image=temp_detection.image,
             mimetype=temp_detection.mimetype,
             filename=temp_detection.filename,
@@ -98,7 +103,8 @@ def save_results():
             mouldy = temp_detection.mouldy,
             insectInfested = temp_detection.insectInfested,
             slaty = temp_detection.slaty,
-            germinated = temp_detection.germinated
+            germinated = temp_detection.germinated,
+            date = temp_detection.date
         )
         # add the new record to the database session and commit the changes
         db.session.add(detection)
@@ -108,10 +114,14 @@ def save_results():
         db.session.commit()
 
         return "Assessment Results Saved", 200
-    
+
+
 @app.route('/detections')
 def get_detections():
-    detections = Detection.query.all()
+
+    user_id = 1
+    detections = Detection.query.filter_by(user_id=user_id).all()
+
     detection_list = [
         {'id': detection.id, \
          'img_src_url': f"http://{FLASK_IP_ADDR}:5000/detections/{detection.filename}", \
@@ -127,20 +137,98 @@ def get_detections():
          'insectInfested': detection.insectInfested, \
          'slaty': detection.slaty,\
          'germinated': detection.germinated, \
-         'beanGrade': detection.beanGrade } for detection in detections]
+         'beanGrade': detection.beanGrade, \
+         'date': detection.date } for detection in detections]
     return jsonify(detection_list)
-    
+
 @app.route('/detections/<string:filename>')
 def get_image(filename):
     detection = Detection.query.filter_by(filename=filename).first()
     if not detection:
         detection = TempDetection.query.filter_by(filename=filename).first()
-    
+
     return send_file(io.BytesIO(detection.image), mimetype=detection.mimetype, download_name=detection.filename)
+
+@app.route('/recent_detections')
+@jwt_required()
+def get_recent_detections():
+
+    user_id = get_jwt_identity()
+
+    query = Detection.query.filter_by(user_id=user_id).order_by(Detection.date.desc())
+
+    # get the first five recent detections
+    records = query[:5]
+
+    detection_list = [
+        {'id': detection.id, \
+         'img_src_url': f"http://{FLASK_IP_ADDR}:5000/detections/{detection.filename}", \
+         'g1': detection.g1, \
+         'g2': detection.g2, \
+         'g3': detection.g3, \
+         'g4': detection.g4, \
+         'veryDarkBrown': detection.veryDarkBrown, \
+         'brown': detection.brown, \
+         'partlyPurple': detection.partlyPurple, \
+         'totalPurple': detection.totalPurple, \
+         'mouldy': detection.mouldy, \
+         'insectInfested': detection.insectInfested, \
+         'slaty': detection.slaty,\
+         'germinated': detection.germinated, \
+         'beanGrade': detection.beanGrade, \
+         'date': detection.date } for detection in records]
+    
+    return jsonify(detection_list)
+
+
+# define endpoint for creating a new user account
+@app.route('/create_user', methods=['POST'])
+def create_user():
+    
+    # get user information from request body
+    first_name = str(request.form["firstName"])
+    last_name = str(request.form["lastName"])
+    username = str(request.form["username"])
+    email = str(request.form["email"])
+    password = generate_password_hash(str(request.form["password"]))    
+
+    # save user data to database
+    new_user = User(
+        first_name=first_name,
+        last_name=last_name,
+        username=username,
+        email=email,
+        password=password
+    )
+
+    # add the new record to the database session and commit the changes
+    db.session.add(new_user)
+    db.session.commit()
+
+    # return success message
+    return jsonify({'message': 'User account created successfully'}), 201
+
+
+@app.route('/login', methods=['POST'])
+def login():
+
+    username = str(request.form.get('username'))
+    password = str(request.form.get('password'))
+
+    # Verify user credentials
+    user = User.query.filter_by(username=username).first()
+    if user and check_password_hash(user.password, password):
+        # Generate access token
+        access_token = create_access_token(identity=user.id)
+        return jsonify({'access_token': access_token, 'status': 200})
+    else:
+        return jsonify({'access_token': '--', 'status': 401})
+
 
 @app.route('/')
 def index():
     return "CAQAO Server"
+
 
 def get_json_response(detection):
     json_response = {
@@ -158,7 +246,8 @@ def get_json_response(detection):
         "insectInfested" : detection.insectInfested,
         "slaty" : detection.slaty,
         "germinated" : detection.germinated,
-        "beanGrade": detection.beanGrade
+        "beanGrade": detection.beanGrade,
+        "date": detection.date
     }
     return jsonify(json_response)
 
@@ -214,21 +303,23 @@ def get_bean_grade(class_count, bean_size):
             num_code = "1"
     else:
             num_code = "2"
-    
+
     if bean_size <= 100:
         letter_code = "A"
     elif (bean_size >= 101) and (bean_size <= 110):
         letter_code = "B"
     else:
         letter_code = "C"
-    
+
     return num_code + letter_code
 
 
 if __name__ == "__main__":
+
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    FLASK_IP_ADDR = ip_address
+
     MAX_DET = 50
-    model = torch.hub.load('ultralytics/yolov5', 'custom', path="best.pt")
     model.max_det = MAX_DET
-    app.run(host=FLASK_IP_ADDR, port=5000, debug=True)
-    # sample request
-    # $ curl -X POST -F image=@test/sample.jpg 'http://192.168.72.32:5000/assess'
+    app.run(host="0.0.0.0", port=5000, debug=True)
--- model.py
+++ model.py
@@ -1,7 +1,17 @@
 from db import db
+from datetime import datetime
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80), unique=True, nullable=False)
+    last_name = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
 
 class Detection(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     image = db.Column(db.LargeBinary, nullable=False)
     filename = db.Column(db.Text, nullable=False)
     mimetype = db.Column(db.Text, nullable=False)
@@ -18,6 +28,7 @@ class Detection(db.Model):
     insectInfested = db.Column(db.Integer, default=0)
     slaty = db.Column(db.Integer, default=0)
     germinated = db.Column(db.Integer, default=0)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
 
 class TempDetection(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -37,3 +48,4 @@ class TempDetection(db.Model):
     insectInfested = db.Column(db.Integer, default=0)
     slaty = db.Column(db.Integer, default=0)
     germinated = db.Column(db.Integer, default=0)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
